e0a6d805f15b3fb35718c65d1299977ac0142f63
Emil Olofsgård <olofsga@kth.se>
2024-10-25 16:29:52 +0200

    Exercise 4

--- gen_src/UserFileReader.java
+++ gen_src/UserFileReader.java
@@ -7,22 +7,23 @@ import java.io.IOException;
 public class UserFileReader {
 
     public Map<String, String> readUsers(String filename) {
-        HashMap<String, String> users = new HashMap<String, String>();
+        HashMap<String, String> userMap = new HashMap<String, String>();
         
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))){
             String line;
             while ((line = reader.readLine()) != null){
                 String[] userDetails = line.split(",");
                 if (userDetails.length == 2) {
-                    users.put(userDetails[0].trim(), userDetails[1].trim()); // Adding trim() to remove potential whitespace
+                    userMap.put(userDetails[0].trim(), userDetails[1].trim()); // Adding trim() to remove potential whitespace
                 } else {
                     System.err.println("Skipping invalid line: " + line);
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error!");
+            System.err.println("Couldn't find file 'user_data.txt");
         }
 
-        return users;
+        return userMap;
     }
 }
